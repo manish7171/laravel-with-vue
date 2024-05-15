3c7b48c40859de281da401a78914a99f411b67b0
manish <manish.sainju@esewatravels.com>
2024-05-15 14:53:19 +0200

    setup.sh

--- setup.sh
+++ setup.sh
@@ -8,55 +8,57 @@ mkdir -p ~/upfront
 cd ~/upfront
 
 # Git clone repository in the "upfront" folder
-#echo ">>> git clone into upfront directory... "
-#git clone https://github.com/manish7171/laravel-with-vue.git .
+echo ">>> git clone into upfront directory... "
+git clone https://github.com/manish7171/laravel-with-vue.git .
 
 # Build containers
 echo ">>> building and running docker containers... "
 cd ~/upfront/docker
-docker compose up --build -d
+docker compose up build --build-arg USER=www-data --build-arg USER_ID=1000 --build-arg GROUP_ID=1000
+docker compose up -d
 cd ~/upfront
 
 # create a datbase
 # Define variables
 echo ">>> Creating Database... "
-CONTAINER_NAME="db_jobleads"
+DB_CONTAINER_NAME="db_jobleads"
 DB_NAME="job_leads"
 DB_USER="root"
 DB_PASS="root"
 
 # Run MySQL client command inside Docker container to create database
-docker exec -i $CONTAINER_NAME mysql -u root -p"$DB_PASS" -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+docker exec -i $DB_CONTAINER_NAME mysql -u root -p"$DB_PASS" -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 
 # Optional: Create a database user and grant privileges
-docker exec -i $CONTAINER_NAME mysql -u root -p"$DB_PASS" -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
-docker exec -i $CONTAINER_NAME mysql -u root -p"$DB_PASS" -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
-docker exec -i $CONTAINER_NAME mysql -u root -p"$DB_PASS" -e "FLUSH PRIVILEGES;"
+docker exec -i $DB_CONTAINER_NAME mysql -u root -p"$DB_PASS" -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+docker exec -i $DB_CONTAINER_NAME mysql -u root -p"$DB_PASS" -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
+docker exec -i $DB_CONTAINER_NAME mysql -u root -p"$DB_PASS" -e "FLUSH PRIVILEGES;"
 
 echo ">>> Database '$DB_NAME' created successfully..."
 
 # Copy contents of .env.example to .env file
 cp ~/upfront/src/.env.example ~/upfront/src/.env
 
+APP_CONTAINER_NAME="app_jobleads"
 # composer install
 echo ">>> Running composer install... "
-docker exec app_jobleads composer install
+docker exec -i $APP_CONTAINER_NAME composer install
 
 #  npm install
 echo ">>> Running npm install... "
-docker exec app_jobleads npm install
+docker exec -i $APP_CONTAINER_NAME npm install
 
 # compile vue files
 echo ">>> Build frontend... "
-docker exec app_jobleads npm run build
+docker exec -i $APP_CONTAINER_NAME npm run build
 
 # build table
 echo ">>> Create User table... "
-docker exec app_jobleads php artisan migrate
+docker exec -i $APP_CONTAINER_NAME php artisan migrate
 
 # seed user table
 echo ">>> Seeding User table... "
-docker exec app_jobleads php artisan db:seed
+docker exec -i $APP_CONTAINER_NAME php artisan db:seed
 
 echo ">>> Open http://localhost:8000/frontend for viewing project"
 echo ">>> Open http://localhost:8888 for viewing database"
