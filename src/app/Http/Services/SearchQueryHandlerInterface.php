416dbdf11519da010069ddd9075ccc0f2248c1ef
manish <manish.sainju@esewatravels.com>
2024-05-17 10:17:47 +0200

    refactor: UserBuilder class handles the search Query now. Modified test as well.

--- src/app/Http/Services/SearchQueryHandlerInterface.php
+++ src/app/Http/Services/SearchQueryHandlerInterface.php
@@ -4,10 +4,9 @@ declare(strict_types=1);
 
 namespace App\Http\Services;
 
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 interface SearchQueryHandlerInterface
 {
-    public function buildQuery(Request $request): Builder;
+    public function handle(Request $request): array;
 }
