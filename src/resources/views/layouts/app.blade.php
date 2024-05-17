daaf08641c14a2fafaea27f034159faf55363b42
manish <manish.sainju@esewatravels.com>
2024-05-17 18:25:46 +0200

    update: Added meta description in HTML head section for better seo score

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -7,6 +7,18 @@
 
     <title>Upfront</title>
 
+    <meta name="description" content="Upfront tasks">
+
+    <meta property="og:title" content="Upfront users">
+    <meta property="og:type" content="UserListing">
+    <meta property="og:url" content="https://www.jobleads.com/">
+    <meta property="og:description" content="Uses Vue as a frontend framework with bootstrap css">
+    <meta property="og:image" content="">
+
+    <!-- Twitter -->
+    <meta property="twitter:title" content="Upfront users">
+    <meta property="twitter:description" content="Uses Vue as a frontend framework with bootstrap css">
+    <meta property="twitter:image" content="">
     <!-- Fonts -->
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 </head>
