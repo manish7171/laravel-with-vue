1749dc665847912dc5349b2267b2e123e1b52eac
manish <manish.sainju@esewatravels.com>
2024-05-17 08:34:47 +0200

    refactor: add spinner while loading. Reduced bootstap css import

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,12 +1,11 @@
 import * as bootstrap from "bootstrap";
 window.bootstrap = bootstrap;
+import axios from "axios";
+window.axios = axios;
+
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 import { createApp } from "vue";
-// import IncrementCounter from "./components/IncrementCounter.vue";
 import router from "./router";
 import App from "./App.vue";
 
 createApp(App).use(router).mount("#app");
-// createApp({})
-//     .use(router)
-//     .component("IncrementCounter", IncrementCounter)
-//     .mount("#app");
