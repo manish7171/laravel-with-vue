790aa8c38d9c221baee58ae9ec22cb6d9bd53d4c
manish <manish7171@gmail.com>
2024-05-21 16:33:00 +0200

    refactor: reponsive icons for Add, Edit and delete buttons

--- src/resources/js/pages/Users.vue
+++ src/resources/js/pages/Users.vue
@@ -319,12 +319,34 @@ const sortButtonClasses = computed(() => (isActive) => {
         <div class="d-flex justify-content-between">
             <!-- Button trigger modal -->
             <button
-                type="button"
-                class="btn btn-primary"
                 data-bs-toggle="modal"
                 data-bs-target="#modal_new_user"
+                type="button"
+                class="btn btn-primary"
             >
-                New User
+                <span class="">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="13"
+                        height="13"
+                        fill="currentColor"
+                        class="bi bi-person-plus-fill"
+                        viewBox="0 0 16 16"
+                    >
+                        <path
+                            d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"
+                        ></path>
+                        <path
+                            fill-rule="evenodd"
+                            d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5"
+                        ></path>
+                    </svg>
+                </span>
+                <span
+                    class="d-none d-sm-none d-md-none d-lg-inline d-xl-inline d-xll-inline"
+                >
+                    New User
+                </span>
             </button>
             <div>
                 <input
@@ -713,13 +735,50 @@ const sortButtonClasses = computed(() => (isActive) => {
                                         class="btn btn-primary"
                                         @click="openEditUserModal(user.id)"
                                     >
-                                        Edit
+                                        <span class="">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                width="13"
+                                                height="13"
+                                                fill="currentColor"
+                                                class="bi bi-pencil-fill"
+                                                viewBox="0 0 16 16"
+                                            >
+                                                <path
+                                                    d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"
+                                                />
+                                            </svg>
+                                        </span>
+
+                                        <span
+                                            class="d-none d-sm-none d-md-none d-lg-inline d-xl-inline d-xll-inline"
+                                        >
+                                            Edit</span
+                                        >
                                     </button>
                                     <button
                                         class="btn btn-danger"
                                         @click="openDeleteUserModal(user.id)"
                                     >
-                                        Delete
+                                        <span class="">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                width="13"
+                                                height="13"
+                                                fill="currentColor"
+                                                class="bi bi-trash3-fill"
+                                                viewBox="0 0 16 16"
+                                            >
+                                                <path
+                                                    d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"
+                                                />
+                                            </svg>
+                                        </span>
+                                        <span
+                                            class="d-none d-sm-none d-md-none d-lg-inline d-xl-inline d-xll-inline"
+                                        >
+                                            Delete</span
+                                        >
                                     </button>
                                 </div>
                             </td>
